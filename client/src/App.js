4b2803aba35ab263a6a842fb89422e4e10567f13
ern19 <aarontrammell19@gmail.com>
2017-11-01 15:53:49 -0400

    fucked with some styling

--- client/src/App.js
+++ client/src/App.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 import NavBar from './components/NavBar.js'
+import styled from 'styled-components'
 
 class App extends Component {
   render() {
--- client/src/components/NavBar.js
+++ client/src/components/NavBar.js
@@ -1,13 +1,30 @@
 import React, { Component } from 'react';
+import styled from 'styled-components'
+
+const StyledNavBar = styled.div` 
+    display: flex;
+    justify-content: space-around;
+    align-items: center;
+    font-size: 1.5rem;
+
+    text-shadow: 1px 1px #2b2b2b;
+    color: rgba(31, 75, 247, .9)
+`
+const StyledAnchors = styled.a`
+    padding: 20px;
+`
 
 class NavBar extends Component {
     render() {
         return (
-            <div>
-                <h1>Vagabond</h1>
-                <a href="#">Sign-Up</a>
-                <a href="#">Log in</a>
-            </div >
+            <StyledNavBar>
+                
+                    <h1>Vagabond</h1>
+                <div><StyledAnchors href="#">Sign-Up</StyledAnchors>
+                    <StyledAnchors href="#">Log in</StyledAnchors></div>
+                    
+                
+            </StyledNavBar>
         );
     }
 }
