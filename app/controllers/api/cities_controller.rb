c9c70780337f15bfb46b211ada3a0febd7aab62f
Hunter Maddux <phmaddux@gmail.com>
2017-11-01 16:58:53 -0400

    routing

--- app/controllers/api/cities_controller.rb
+++ app/controllers/api/cities_controller.rb
@@ -3,6 +3,33 @@ class Api::CitiesController < ApplicationController
         @cities = City.all 
         render json: @cities
     end
-    
+    def show
+        @city = City.find(params[:id])
+        render json: @city
+    end
+    def create
+        @acity = City.create!(city_params)
+        render json: @city
+    end
+    def
+        
+    end
+    def
+        
+    end
+    end
+    def
+        
+    end
+    def
+        
+    end
+
+    private
+
+    def
+        
+    end
+
     
 end
