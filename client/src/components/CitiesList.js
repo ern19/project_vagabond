2233073801269e17f4b2ae9a5b8dfa6e6c127e4d
ern19 <aarontrammell19@gmail.com>
2017-11-01 21:10:10 -0400

    added descriptions to cards

--- client/src/components/CitiesList.js
+++ client/src/components/CitiesList.js
@@ -10,15 +10,12 @@ class CitiesList extends Component {
                         return (
                             <Card>
                                 <CardMedia
-                                overlay={<CardTitle title={city.name} />}
-                                >
-                                <img src={city.photo_url} alt="city pic" />
+                                    overlay={<CardTitle title={city.name} />}
+                                    >
+                                    <img src={city.photo_url} alt="city pic" />
                                 </CardMedia>
                                 <CardText>
-                                Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-                                Donec mattis pretium massa. Aliquam erat volutpat. Nulla facilisi.
-                                Donec vulputate interdum sollicitudin. Nunc lacinia auctor quam sed pellentesque.
-                                Aliquam dui mauris, mattis quis lacus id, pellentesque lobortis odio.
+                                    {city.description}
                                 </CardText>
                             </Card>
                         )
