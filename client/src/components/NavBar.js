10c44c26a1652ee185cbb86ee3df2ccd60b20234
ern19 <aarontrammell19@gmail.com>
2017-11-02 15:36:35 -0400

    added some styles and stuff

--- client/src/components/NavBar.js
+++ client/src/components/NavBar.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import styled from 'styled-components'
+import {Link} from 'react-router-dom'
 
 const StyledNavBar = styled.div` 
     display: flex;
@@ -12,14 +13,22 @@ const StyledNavBar = styled.div`
 `
 const StyledAnchors = styled.a`
     padding: 20px;
+    color: #747578;
+    text-shadow: 1px 1px #72E0FF;
 `
 
+const HeaderLink = styled.a`
+    text-decoration: none;
+    color: #747578;
+    text-shadow: 2px 2px #72E0FF;
+
+`
 class NavBar extends Component {
     render() {
         return (
             <StyledNavBar>
                 
-                    <h1>Vagabond</h1>
+                    <h1><HeaderLink href={'/'}>Vagabond</HeaderLink></h1>
                 <div><StyledAnchors href="#">Sign-Up</StyledAnchors>
                     <StyledAnchors href="#">Log in</StyledAnchors></div>
                     
