dc46c21f5bc52a34e8f024fe20aea0c4cd83fea3
ern19 <aarontrammell19@gmail.com>
2017-11-01 21:07:22 -0400

    added city cards

--- client/src/components/CitiesList.js
+++ client/src/components/CitiesList.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import {Card, CardActions, CardHeader, CardMedia, CardTitle, CardText} from 'material-ui/Card'
 
 class CitiesList extends Component {
     render(props) {
@@ -7,9 +8,19 @@ class CitiesList extends Component {
                 {
                     this.props.cities.map((city) => {
                         return (
-                            <div>
-                                <p>{city.name}</p>
-                            </div>
+                            <Card>
+                                <CardMedia
+                                overlay={<CardTitle title={city.name} />}
+                                >
+                                <img src={city.photo_url} alt="city pic" />
+                                </CardMedia>
+                                <CardText>
+                                Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                                Donec mattis pretium massa. Aliquam erat volutpat. Nulla facilisi.
+                                Donec vulputate interdum sollicitudin. Nunc lacinia auctor quam sed pellentesque.
+                                Aliquam dui mauris, mattis quis lacus id, pellentesque lobortis odio.
+                                </CardText>
+                            </Card>
                         )
                     })
                 }
