5c09a14d2a96aedc0dd211a556035c06c26131fc
Hunter Maddux <phmaddux@gmail.com>
2017-11-03 09:56:24 -0400

    Post commented

--- client/src/components/Post.js
+++ client/src/components/Post.js
@@ -5,41 +5,51 @@ import styled from 'styled-components'
 import {Card, CardActions, CardHeader, CardText} from 'material-ui/Card';
 import FlatButton from 'material-ui/FlatButton';
 
-
-
-
 class Post extends Component {
-
+    // this sets the state of the 'post' object
     state = {
         post: {},
         redirectToCity: false
     }
 
+    // Component Will Mount makes sure this all happens before the page is rendered
     async componentWillMount() {
         try {
+            //  this sets the variables from the passed down cityId and postId and then
             const cityId = this.props.match.params.cityId
             const postId = this.props.match.params.postId
 
+            // requests the information on each Id from our API with Axios
             const response = await axios.get(`/api/cities/${cityId}/posts/${postId}`)
+            
+            // this logs all the response data to the console
             console.table(response.data)
+            // this is used to set the state to match the data in the response from the API
             this.setState({post: response.data})
         } catch (error) {
+            // This catches and logs any errors
             console.log(error)
         }
     }
+    // this deletes a post if triggered
     deletePost = async () => {
+        // this opens a confirm window asking the user to confirm if the delete button is pressed
         if(window.confirm(`Are you sure you want to delete ${this.state.post.title}?`)) {
+            //  this sets the variables from the passed down cityId and postId and then
             const cityId = this.props.match.params.cityId
             const postId = this.props.match.params.postId
             
+            // this requests from axios the specific post located at cityId and at postId
             const res = await axios.delete(`/api/cities/${cityId}/posts/${postId}`)
             this.setState({post: res.data, redirectToCity: true})  
         }
     }
     render() {
+        //  this sets the variables from the passed down cityId and postId and then
         const cityId = this.props.match.params.cityId
         const postId = this.props.match.params.postId
         if (this.state.redirectToCity) {
+            // this redirects to city if the previous condition is triggered
             return <Redirect to ={`/cities/${cityId}`}/>
         }
         return (
