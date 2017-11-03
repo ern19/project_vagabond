695f0323b81a44074542769950b006d5e1d8ab48
Hunter Maddux <phmaddux@gmail.com>
2017-11-03 09:05:13 -0400

    City Commented

--- client/src/components/CitiesList.js
+++ client/src/components/CitiesList.js
@@ -6,6 +6,7 @@ import styled from "styled-components";
 
 
 class CitiesList extends Component {
+
     render() {
         return (
             <div>
--- client/src/components/City.js
+++ client/src/components/City.js
@@ -5,33 +5,47 @@ import PostList from './PostList'
 import {Link} from 'react-router-dom'
 import NewPostForm from './NewPostForm'
 class City extends Component {
-
+     // this sets the state of the 'city' object and the 'posts' array 
     state = {
         city: {},
         posts: [],
     }
-
+    // Component Will Mount makes sure this all happens before the page is rendered
     async componentWillMount() {
         try {
+            //  this passes down the cityId and then
             const cityId = this.props.match.params.cityId
+            // requests the information on that Id from our API with Axios
             const response = await axios.get(`/api/cities/${cityId}`)
             
+            // this is used to set the state to match the data in the response from the API
             this.setState({city: response.data})
         } catch (error) {
+            // This catches and logs any errors
             console.log(error)
         }
         try {
+            //  this passes down the cityId and then
             const cityId = this.props.match.params.cityId
+            // requests the information specifically about posts on that Id from our API with Axios
             const response = await axios.get(`/api/cities/${cityId}/posts`)
+            // this puts a readable table of data into the console to preview the response data
             console.table(response.data)
+
+            // this is used to set the state to match the data in the response from the API
             this.setState({posts: response.data})
+
+            // this puts the data into the console to preview the state data
             console.log(this.state)
         } catch (error) {
+            // This catches and logs any errors
             console.log(error)
         }
     }
     render() {
-        const cityId = this.props.match.params.cityId        
+        // this sets the variable of the city we are displaying to match what is in the url
+        const cityId = this.props.match.params.cityId 
+        // this returns all the data on the city and the posts related to that city       
             return (
                 <div>
                     
